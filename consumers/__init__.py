a524804f15b93c5c10d61d793c0c7788e2f05598
mattsel <matt.sel04@gmail.com>
2025-03-22 14:40:42 -0400

    Added thread count arg

--- consumers/__init__.py
+++ consumers/__init__.py
@@ -3,16 +3,29 @@ from app import main
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="List of brokers")
-    
     parser.add_argument(
-        '--list', 
-        type=str, 
+        '--brokers',
+        type=str,
         nargs='+',
-        help='A list of brokers to connect to ', 
+        help='A list of brokers to connect to',
         required=True
     )
-    
-    args = parser.parse_args()
-    
-    main(args.list)
+    parser.add_argument(
+        '--batch',
+        type=int,
+        help='Batch size to send to ELK',
+        required=False
+    )
+    parser.add_argument(
+        '--threads',
+        type=int,
+        help='Amount of threads to process topics',
+        required=False
+    )
 
+    args = parser.parse_args()
+   
+    batch = args.batch if args.batch is not None else 5000
+    threads = args.thread if args.thread is not None else 15
+    
+    main(args.brokers, batch, threads)
--- consumers/app.py
+++ consumers/app.py
@@ -1,7 +1,10 @@
+import threading
 from typing import List
 from KafkaClient import KafkaClient
 
 
-def main(brokers: List[str]) -> None:
+def main(brokers: List[str], batch: int, threads: int) -> None:
     kafka_client = KafkaClient(brokers)
+    topics = kafka_client.get_topics()
+
 
